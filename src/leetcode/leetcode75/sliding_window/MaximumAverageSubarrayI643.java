15c11420868f1362e51776acac806f469f89fc0c
keeen <beisenbaynurtilek5@gmail.com>
2024-03-18 00:19:44 +0500

    Leetcode task solved: 643 Maximum Average Subarray I

---
+++ src/leetcode/leetcode75/sliding_window/MaximumAverageSubarrayI643.java
@@ -0,0 +1,26 @@
+package leetcode.leetcode75.sliding_window;
+
+public class MaximumAverageSubarrayI643 {
+    public static void main(String[] args) {
+        MaximumAverageSubarrayI643 i = new MaximumAverageSubarrayI643();
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 4;
+        System.out.println(i.findMaxAverage(nums, k));
+    }
+
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int left = 0, right = k, max = sum;
+        while (right < nums.length){
+            sum = sum - nums[left];
+            left++;
+            sum = sum + nums[right];
+            right++;
+            max = Math.max(max, sum);
+        }
+        return (double) max/k;
+    }
+}
